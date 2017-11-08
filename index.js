d6cf2587421830272f71dbc3e770db05ee256481
season.chen <season.chen.i@foxmail.com>
2017-11-08 23:47:07 +0800

    cacheID

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ const uuidv4 = require('uuid/v4');
 
 module.exports = class FileStore {
   constructor(opts={}) {
-    this.cacheId = opts.cacheId || uuidv4();
     this.cacheDir = opts.cacheDir;
-    this.maxAge = opts.maxAge || 86400000; //, 1 day
-    // cache
-    this.cache = flatCache.load(this.cacheId, this.cacheDir);
+    this.maxAge = opts.maxAge || 86400000; //ms, 1 day
   }
 
   get(key, maxAge, { rolling }) {
-    let data = this.cache.getKey(key);
+    let cache = flatCache.load(key, this.cacheDir);
+    let data = cache.getKey(key);
     // no session
     if (!data || !data.session) return null;
     // session expired
@@ -21,15 +19,16 @@ module.exports = class FileStore {
   }
 
   set(key, sess, maxAge, { rolling, changed }) {
+    let cache = flatCache.load(key, this.cacheDir);
     let data = {};
 
     data.session = sess;
     data.expired = Date.now() + (maxAge || this.maxAge);
-    this.cache.setKey(key, data);
-    this.cache.save();
+    cache.setKey(key, data);
+    cache.save();
   }
 
   destroy(key) {
-    this.cache.removeKey('key');
+    flatCache.clearCacheById(key);
   }
 }
