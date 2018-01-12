5bd960c310ec3473b4aa744aee467a377ab524d0
season <season@season.local>
2018-01-13 01:05:42 +0800

    1.0.4

--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 
 > a FileStore use [flat-cache](https://github.com/royriojas/flat-cache) for [koa-session](https://github.com/koajs/session).
 
+## Suggested to use for:
+
+- The amount of your site's users is not very large.
+- Has sensitive session contents that can not store in cookie (e.g. some tokens).
+
+## Not suggested to use for:
+
+- The amount of your site's users is very large.
+- No sensitive session contents that can not store in cookie.
+
 ## requirement
 
 - node.js@8.x (es6)
@@ -16,11 +26,34 @@ const session = require('koa-session');
 const path = require('path');
 const app = new Koa();
 
-app.use(session({
+app.use(session(app, {
   // other session config
   store: new FileStore({
     cacheDir: path.resolve(__dirname, './session/'), // Dir to store session files, unencrypted.
     maxAge: 86400000, // Default maxAge for session, used if the maxAge of `koa-session` is undefined, avoid the session's key in cookies is stolen.
   })
-}, app));
+}));
 ```
+
+## Tips
+
+Every `session key` will generate a file in the `cacheDir`, so maybe you will clean the old files after some days.
+
+You can make a `timed task` of `shell script` to clean them (e.g. delete files that last update is before 30 days).
+
+some example, for reference only:
+
+/root/soft_shell/clean-cache.sh:
+
+```sh
+#!/bin/sh
+find /app/cacheDir/ -mtime +30 -name "*.*" -exec rm -Rf {} \;
+```
+
+crontab:
+
+```sh
+crontab -l
+10 4 1 * * /bin/sh /root/soft_shell/clean-cache.sh
+```
+
--- index.js
+++ index.js
@@ -1,34 +1,58 @@
 const flatCache = require('flat-cache');
-const uuidv4 = require('uuid/v4');
 
 module.exports = class FileStore {
   constructor(opts={}) {
+    if (!opts.cacheDir) {
+      throw new Error('no cacheDir');
+    }
+
     this.cacheDir = opts.cacheDir;
     this.maxAge = opts.maxAge || 86400000; //ms, 1 day
   }
 
-  get(key, maxAge, { rolling }) {
+  async get(key, maxAge, { rolling }) {
     let cache = flatCache.load(key, this.cacheDir);
-    let data = cache.getKey(key);
+    let session = cache.getKey('session') || null;
+    let expired = cache.getKey('expired') || null;
+
+    // expired
+    if (expired && expired < Date.now()) {
+      return null;
+    }
+
     // no session
-    if (!data || !data.session) return null;
-    // session expired
-    if (data.expired < Date.now() + (maxAge || 0)) return null;
-    // session
-    return data.session;
+    if (!session) return null;
+
+    // reset expired
+    if (rolling) {
+      cache.setKey('expired', Date.now() + (maxAge || this.maxAge));
+      cache.save();
+    }
+    
+    // this session
+    return session;
   }
 
-  set(key, sess, maxAge, { rolling, changed }) {
+  async set(key, sess, maxAge, { rolling, changed }) {
     let cache = flatCache.load(key, this.cacheDir);
-    let data = {};
+    let expired = cache.getKey('expired') || null;
 
-    data.session = sess;
-    data.expired = Date.now() + (maxAge || this.maxAge);
-    cache.setKey(key, data);
+    // no change
+    if (expired && !rolling && !changed) return;
+
+    // reset expired
+    if (!expired || rolling) {
+      cache.setKey('expired', Date.now() + (maxAge || this.maxAge));
+    }
+
+    // set session
+    cache.setKey('session', sess);
+
+    // save to file
     cache.save();
   }
 
-  destroy(key) {
+  async destroy(key) {
     flatCache.clearCacheById(key);
   }
 }
